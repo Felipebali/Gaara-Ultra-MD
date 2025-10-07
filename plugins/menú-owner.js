f169e6fb4cb181a420f7862e4ae9554ecf41954b
Felipebali <felipebaliski@gmail.com>
2025-10-07 11:30:25 -0300

    Update menú-owner.js

--- plugins/menú-owner.js
+++ plugins/menú-owner.js
@@ -1,56 +1,39 @@
-// plugins/menuowner.js
-export default {
-    name: 'menuowner',
-    description: 'Menú de comandos exclusivos del Owner 🐱',
-    group: false,
-    all: async function (m, { conn }) {
-        try {
-            const menuText = `
-╭━━━〔 👑 MENÚ OWNER 𝗙𝗘𝗟𝗜𝗖𝗔𝗧 🐱 〕━━━⬣
+// plugins/menu-owner.js
+let handler = async (m) => {
+    try {
+        let menuText = `
+╭━━━〔 👑 MENÚ OWNER 𝗙𝗘𝗟𝗜𝗖𝗔𝗧 🐾 〕━━━⬣
 ┃ ❒ *Comandos exclusivos del dueño*
 ╰━━━━━━━━━━━━━━━━━━━━⬣
 
-╭━━━〔 🐾 ADMIN AUTOMÁTICO 〕━━━⬣
-┃ 🐱 .autoadmin - Activar autoadmin
-╰━━━━━━━━━━━━━━━━━━━━⬣
-
-╭━━━〔 🔨 BAN / UNBAN 〕━━━⬣
-┃ 🐱 .banuser <@user> - Banear usuario
-┃ 🐱 .unbanuser <@user> - Desbanear usuario
-╰━━━━━━━━━━━━━━━━━━━━⬣
-
-╭━━━〔 ⚡ CHEATS 〕━━━⬣
-┃ 🐱 .chetar <@user> - Dar privilegios extra
-┃ 🐱 .deschetar <@user> - Quitar privilegios
-┃ 🐱 .dsowner <@user> - Desactivar dueño temporal
-╰━━━━━━━━━━━━━━━━━━━━⬣
-
-╭━━━〔 🌐 RED / BOT 〕━━━⬣
-┃ 🐱 .join <link> - Unirse a grupo
-┃ 🐱 .restart - Reiniciar bot
-┃ 🐱 .exec <código> - Ejecutar JS
-┃ 🐱 .exec2 <código> - Ejecutar JS avanzado
-╰━━━━━━━━━━━━━━━━━━━━⬣
-
-╭━━━〔 ⚙ CONFIGURACIÓN 〕━━━⬣
-┃ 🐱 .setcmd <comando> - Configurar comando
-┃ 🐱 .setprefix <prefijo> - Cambiar prefijo
-┃ 🐱 .update - Actualizar bot
+╭━━━〔 ⚡ ADMIN / SUB-BOTS 〕━━━⬣
+┃ 🐾 .autoadmin - Activar autoadmin
+┃ 🐾 .banuser <@user> - Banear usuario
+┃ 🐾 .unbanuser <@user> - Desbanear usuario
+┃ 🐾 .chetar - Dar poderes
+┃ 🐾 .deschetar - Quitar poderes
+┃ 🐾 .dsowner - Eliminar dueño
+┃ 🐾 .join <link> - Unirse a grupo
+┃ 🐾 .restart - Reiniciar bot
+┃ 🐾 .exec <comando> - Ejecutar código
+┃ 🐾 .exec2 <comando> - Ejecutar código avanzado
+┃ 🐾 .setcmd - Configurar comando
+┃ 🐾 .setprefix - Cambiar prefijo
+┃ 🐾 .update - Actualizar bot
 ╰━━━━━━━━━━━━━━━━━━━━⬣
 
 > 👑 Powered by FelixCat 🐾
-`;
+        `;
 
-            await conn.sendMessage(m.chat, { text: menuText }, { quoted: m });
+        await conn.sendMessage(m.chat, { text: menuText }, { quoted: m });
 
-        } catch (e) {
-            console.error(e);
-            conn.reply(m.chat, '✖️ Error al mostrar el menú Owner.', m);
-        }
+    } catch (e) {
+        console.error(e);
+        await m.reply('✖️ Error al mostrar el menú de owner.');
     }
-};
+}
 
-export const handler = {
-    command: ['menuow'], // comando para activar el menú
-    owner: true // solo el owner puede usarlo
-};
+handler.command = ['menuow'];
+handler.owner = true;
+
+export default handler;
