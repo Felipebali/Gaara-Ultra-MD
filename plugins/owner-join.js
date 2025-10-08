b94ce62cbc5ce29aa9b53d6e4cc2819b39305d79
Felipebali <felipebaliski@gmail.com>
2025-10-08 11:08:51 -0300

    Update owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -1,7 +1,7 @@
 let linkRegex = /https:\/\/chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i;
 
 let handler = async (m, { conn, text, isOwner }) => {
-    if (!text) return m.reply('⚡️ Debes enviar una invitacion para que *GAARA-ULTRA-MD* se una al grupo.');
+    if (!text) return m.reply('⚡️ Debes enviar una invitacion para que *FELIXCAT-BOT* se una al grupo.');
 
     let [_, code] = text.match(linkRegex) || [];
 
@@ -13,7 +13,7 @@ let handler = async (m, { conn, text, isOwner }) => {
             .catch(err => m.reply(`⚠️ Error al unirme al grupo.`));
     } else {
         let message = `🔰 Invitación a un grupo:\n${text}\n\nPor: @${m.sender.split('@')[0]}`;
-        await conn.sendMessage('584120346669' + '@s.whatsapp.net', { text: message, mentions: [m.sender] }, { quoted: m });
+        await conn.sendMessage('59898719147' + '@s.whatsapp.net', { text: message, mentions: [m.sender] }, { quoted: m });
         m.reply(`🚀 El link del grupo ha sido enviado, gracias por tu invitacion ⚡️`);
     }
 };
