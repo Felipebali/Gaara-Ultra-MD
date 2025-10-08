89f43c28478606c06f391a5515483c7ddaf72a10
Felipebali <felipebaliski@gmail.com>
2025-10-08 19:44:06 -0300

    Update owner-resetuser.js

--- plugins/owner-resetuser.js
+++ plugins/owner-resetuser.js
@@ -12,21 +12,21 @@ const handler = async (m, { conn, text }) => {
     } else if (m.quoted && m.quoted.sender) {
         user = m.quoted.sender;
     } else {
-        return conn.sendMessage(m.chat, `${emoji} Formato de usuario no reconocido. Responda a un mensaje, etiquete a un usuario o escriba su número de usuario.`);
+        return conn.sendMessage(m.chat, { text: `${emoji} Formato de usuario no reconocido. Responda a un mensaje, etiquete a un usuario o escriba su número de usuario.` });
     }
 
     const userNumber = user.split('@')[0];
 
     // Verificar que el usuario exista en la base de datos
     if (!global.db.data.users[user]) {
-        return conn.sendMessage(m.chat, `${emoji} El usuario @${userNumber} no se encuentra en mi base de datos.`, { mentions: [user] });
+        return conn.sendMessage(m.chat, { text: `${emoji} El usuario @${userNumber} no se encuentra en mi base de datos.`, mentions: [user] });
     }
 
     // Eliminar todos los datos del usuario
     delete global.db.data.users[user];
 
     // Mensaje de éxito
-    conn.sendMessage(m.chat, `${done} Éxito. Todos los datos del usuario @${userNumber} fueron eliminados de mi base de datos.`, { mentions: [user] });
+    conn.sendMessage(m.chat, { text: `${done} Éxito. Todos los datos del usuario @${userNumber} fueron eliminados de mi base de datos.`, mentions: [user] });
 };
 
 handler.tags = ['owner'];
