162899d375259961786d8165cadb2c8600e2756f
Felipebali <felipebaliski@gmail.com>
2025-10-08 08:06:41 -0300

    Create grupo-antitemu.js

---
+++ plugins/grupo-antitemu.js
@@ -0,0 +1,17 @@
+// plugins/antitemu.js
+let handler = async (m, { conn }) => {
+    if (!m.isGroup) return m.reply('❌ Este comando solo funciona en grupos.');
+    if (!global.db.data.chats[m.chat]) global.db.data.chats[m.chat] = {};
+    
+    let chat = global.db.data.chats[m.chat];
+    chat.antitemu = !chat.antitemu;
+    
+    m.reply(`✅ El *antitemu* ha sido ${chat.antitemu ? '🟢 activado' : '🔴 desactivado'}.`);
+};
+
+handler.command = ['antitemu'];
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
