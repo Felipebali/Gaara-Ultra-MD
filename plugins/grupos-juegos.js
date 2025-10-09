4c7776452b5c7a9b2eb12cc6a63c4eb70f199157
Felipebali <felipebaliski@gmail.com>
2025-10-09 09:22:18 -0300

    Update and rename juegos-maldad.js to grupos-juegos.js

---
+++ plugins/grupos-juegos.js
@@ -0,0 +1,24 @@
+// plugins/juegos.js
+let handler = async (m, { conn }) => {
+try {
+const chat = global.db.data.chats[m.chat] || {};
+chat.games = !chat.games; // cambia el estado
+global.db.data.chats[m.chat] = chat;
+
+const estado = chat.games ? '🟢 Activados' : '🔴 Desactivados';  
+    await conn.sendMessage(m.chat, { text: `🎮 Mini-juegos ${estado} en este chat.` });  
+} catch (e) {  
+    console.error(e);  
+    await m.reply('✖️ Error al cambiar el estado de los juegos.');  
+}
+
+};
+
+handler.command = ['juegos'];
+handler.group = true;
+handler.admin = true;
+
+export default handler;
+
+Esto hace que mantenga los juegos activados o desactivados
+
