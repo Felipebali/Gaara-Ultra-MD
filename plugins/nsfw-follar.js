9be666ef5847558a58878fe87208341d56b8cfde
Felipebali <felipebaliski@gmail.com>
2025-10-11 23:15:15 -0300

    Update nsfw-follar.js

--- plugins/nsfw-follar.js
+++ plugins/nsfw-follar.js
@@ -14,23 +14,24 @@ let handler = async (m, { conn }) => {
 
     // Detectamos el usuario mencionado o citado
     let who;
-    if (m.mentionedJid.length > 0) who = m.mentionedJid[0];
+    if (m.mentionedJid?.length > 0) who = m.mentionedJid[0];
     else if (m.quoted) who = m.quoted.sender;
     else who = m.sender;
 
-    let name = conn.getName(who);
-    let name2 = conn.getName(m.sender);
+    // Solo usernames
+    const usernameTarget = `@${who.split("@")[0]}`;
+    const usernameSender = `@${m.sender.split("@")[0]}`;
 
     m.react('🥵');
 
     // Construimos el mensaje según si hay mención o cita
     let str;
-    if (m.mentionedJid.length > 0) {
-        str = `\`${name2}\` *follo fuertemente a la perra de* \`${name || who}\`.`;
+    if (m.mentionedJid?.length > 0) {
+        str = `${usernameSender} *follo fuertemente a la perra de* ${usernameTarget}.`;
     } else if (m.quoted) {
-        str = `\`${name2}\` *se la metió durísimo a la perrita de* \`${name || who}\`.`;
+        str = `${usernameSender} *se la metió durísimo a la perrita de* ${usernameTarget}.`;
     } else {
-        str = `\`${name2}\` *está follando ricamente.*`.trim();
+        str = `${usernameSender} *está follando ricamente.*`.trim();
     }
 
     if (m.isGroup) {
@@ -44,7 +45,8 @@ let handler = async (m, { conn }) => {
         ];
         const video = videos[Math.floor(Math.random() * videos.length)];
 
-        let mentions = [who];
+        // Menciones: sender y target
+        const mentions = [m.sender, who];
         conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, mentions }, { quoted: m });
     }
 }
