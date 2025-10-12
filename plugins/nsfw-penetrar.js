58d442a6e44228b1c6cbe938d4e9a00beb2f890b
Felipebali <felipebaliski@gmail.com>
2025-10-11 23:18:04 -0300

    Update nsfw-penetrar.js

--- plugins/nsfw-penetrar.js
+++ plugins/nsfw-penetrar.js
@@ -1,22 +1,26 @@
 let handler = async (m, { conn, command, text }) => {
     if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
+        return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +18 están desactivados en este grupo.\n> Si eres admin y deseas activarlos usa .enable nsfw');
     }
+
     // Obtiene el usuario mencionado o el que respondió al mensaje
-    let user = m.mentionedJid[0] || (m.quoted ? m.quoted.sender : m.sender);
-    let userName = user === m.sender ? `@${m.sender.split('@')[0]}` : `@${user.split('@')[0]}`;
+    let user = m.mentionedJid?.[0] || (m.quoted ? m.quoted.sender : m.sender);
+
+    // Solo usernames de sender y target
+    const usernameTarget = `@${user.split('@')[0]}`;
+    const usernameSender = `@${m.sender.split('@')[0]}`;
 
     // Mensaje de respuesta
     const responseMessage = `
 *TE HAN LLENADO LA CARA DE SEMEN POR PUTA Y ZORRA!*
 
-*Le ha metido el pene a ${text || userName}* con todo y condón hasta quedar seco, has dicho "por favor más duroooooo!, ahhhhhhh, ahhhhhh, hazme un hijo que sea igual de pitudo que tú!" mientras te penetraba y luego te ha dejado en silla de ruedas!
+*Le ha metido el pene a ${text || usernameTarget}* con todo y condón hasta quedar seco, has dicho "por favor más duroooooo!, ahhhhhhh, ahhhhhh, hazme un hijo que sea igual de pitudo que tú!" mientras te penetraba y luego te ha dejado en silla de ruedas!
 
-*${text || userName}* 
+*${text || usernameTarget}* 
 🔥 *YA TE HAN PENETRADO!*`;
 
-    // Envía la respuesta al chat
-    conn.reply(m.chat, responseMessage, null, { mentions: [user] });
+    // Envía la respuesta al chat mencionando ambos
+    conn.reply(m.chat, responseMessage, null, { mentions: [m.sender, user] });
 }
 
 // Ayuda y configuración del comando
