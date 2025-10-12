23c8d91baa6fdcf6099892c7033b7036f7fe98d3
Felipebali <felipebaliski@gmail.com>
2025-10-11 23:21:15 -0300

    Update nsfw-tetas.js

--- plugins/nsfw-tetas.js
+++ plugins/nsfw-tetas.js
@@ -1,15 +1,28 @@
+// plugins/nsfw-boobs.js
 let handler = async (m, { conn }) => {
-    let chat = global.db.data.chats[m.chat];
-    if (!chat?.nsfw) return; // ⚠️ Sale si NSFW está desactivado
+    const chat = global.db.data.chats[m.chat] || {};
+    if (!chat.nsfw) return; // ⚠️ Sale si NSFW está desactivado
 
+    // Username del sender
+    const usernameSender = `@${m.sender.split("@")[0]}`;
+
+    // Imagen y caption
     let img = 'https://delirius-apiofc.vercel.app/nsfw/boobs';
-    let text = '*🫨 TETAS*';
+    let text = `*🫨 TETAS de ${usernameSender}*`;
 
-    conn.sendMessage(m.chat, { image: { url: img }, caption: text }, { quoted: m });
+    // Enviar imagen con mención
+    await conn.sendMessage(
+        m.chat,
+        { image: { url: img }, caption: text, mentions: [m.sender] },
+        { quoted: m }
+    );
+
     m.react('✅');
 };
 
 handler.help = ['tetas'];
 handler.command = ['tetas'];
 handler.tags = ['nsfw'];
+handler.group = true;
+
 export default handler;
