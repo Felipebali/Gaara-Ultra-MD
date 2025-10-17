8f2acaa29ec80a28d8e917f151bc2bb5a2fcf64d
Felipebali <felipebaliski@gmail.com>
2025-10-17 12:03:02 -0300

    Update grupos-Tag.js

--- plugins/grupos-Tag.js
+++ plugins/grupos-Tag.js
@@ -1,9 +1,11 @@
-import { generateWAMMessageFromContent } from '@whiskeysockets/baileys';
+// plugins/grupos-Tag.js
+import baileys from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent } = baileys;
 import * as fs from 'fs';
 
 const handler = async (m, { conn, text, participants }) => {
   try {
-    const users = participants.map((u) => conn.decodeJid(u.id));
+    const users = participants.map(u => conn.decodeJid(u.id));
     const q = m.quoted ? m.quoted : m || m.text || m.sender;
     const c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender;
 
@@ -12,7 +14,7 @@ const handler = async (m, { conn, text, participants }) => {
       generateWAMessageFromContent(
         m.chat,
         { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c } },
-        { userJid: conn.user.id } // ← removido quoted
+        { userJid: conn.user.id } // no citado
       ),
       text || q.text,
       conn.user.jid,
@@ -22,7 +24,7 @@ const handler = async (m, { conn, text, participants }) => {
     await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
 
   } catch {
-    const users = participants.map((u) => conn.decodeJid(u.id));
+    const users = participants.map(u => conn.decodeJid(u.id));
     const quoted = m.quoted ? m.quoted : m;
     const mime = (quoted.msg || quoted).mimetype || '';
     const isMedia = /image|video|sticker|audio/.test(mime);
@@ -32,16 +34,16 @@ const handler = async (m, { conn, text, participants }) => {
 
     if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
       const mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, { image: mediax, caption: htextos, mentions: users }); // sin quoted
+      conn.sendMessage(m.chat, { image: mediax, caption: htextos, mentions: users });
     } else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
       const mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, { video: mediax, mimetype: 'video/mp4', caption: htextos, mentions: users }); // sin quoted
+      conn.sendMessage(m.chat, { video: mediax, mimetype: 'video/mp4', caption: htextos, mentions: users });
     } else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
       const mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, { audio: mediax, mimetype: 'audio/mpeg', fileName: `Hidetag.mp3`, mentions: users }); // sin quoted
+      conn.sendMessage(m.chat, { audio: mediax, mimetype: 'audio/mpeg', fileName: `Hidetag.mp3`, mentions: users });
     } else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
       const mediax = await quoted.download?.();
-      conn.sendMessage(m.chat, { sticker: mediax, mentions: users }); // sin quoted
+      conn.sendMessage(m.chat, { sticker: mediax, mentions: users });
     } else {
       await conn.relayMessage(
         m.chat,
@@ -58,7 +60,7 @@ const handler = async (m, { conn, text, participants }) => {
           }
         },
         {}
-      ); // sin quoted
+      );
     }
   }
 };
