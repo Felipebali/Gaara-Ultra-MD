468d170cf806908c9e93a3caff0c0bb610ea740c
Felipebali <felipebaliski@gmail.com>
2025-10-18 10:41:46 -0300

    Update menú-owner.js

--- plugins/menú-owner.js
+++ plugins/menú-owner.js
@@ -1,33 +1,31 @@
 // plugins/menu-owner.js
 let handler = async (m, { conn }) => {
   try {
-    let menuText = `
-╭━━━━━━━━━━━━━━━━━━━━╮
+    let menuText = `╭━━━━━━━━━━━━━━━━━━━━╮
 │ 👑 *MENÚ OWNER* 👑
 │ Comandos exclusivos del dueño
 ╰━━━━━━━━━━━━━━━━━━━━╯
-
-🐾 .autoadmin         - Otorga poderes de administrador
-🐾 .banuser <@user>   - Banear usuario
+📌 *Gestión de Admins*
+🐾 .autoadmin - Otorga poderes de administrador
+🐾 .chetar - Concede habilidades especiales
+🐾 .deschetar - Quita habilidades
+📌 *Usuarios*
+🐾 .banuser <@user> - Banear usuario
 🐾 .unbanuser <@user> - Desbanear usuario
-🐾 .chetar            - Concede habilidades especiales
-🐾 .deschetar         - Quita habilidades
-🐾 .dsowner           - Eliminar dueño
-🐾 .join <link>       - Unirse a un grupo
-🐾 .restart           - Reiniciar bot
-🐾 .exec <comando>    - Ejecutar código
-🐾 .exec2 <comando>   - Ejecutar código avanzado
-🐾 .setcmd            - Configurar comando
-🐾 .setprefix         - Cambiar prefijo
-🐾 .update            - Actualizar bot
 🐾 .resetuser <@user> - Borrar todos los datos de un usuario
+📌 *Bot*
+🐾 .restart - Reiniciar bot
+🐾 .update - Actualizar bot
+🐾 .exec <comando> - Ejecutar código
+🐾 .exec2 <comando> - Ejecutar código avanzado
+🐾 .setcmd - Configurar comando
+🐾 .setprefix - Cambiar prefijo
+🐾 .dsowner - Eliminar dueño
+🐾 .join <link> - Unirse a un grupo
+✨ ⚡ FelixCat - Owner`;
 
-✨ ⚡ FelixCat - Owner
-`;
-
-    // Enviar mensaje con imagen de portada
     await conn.sendMessage(m.chat, {
-      image: { url: 'https://files.catbox.moe/3ryutp.jpg' }, // Reemplazar con la URL de tu imagen de owner
+      image: { url: 'https://files.catbox.moe/3ryutp.jpg' }, // URL de imagen del Owner
       caption: menuText
     }, { quoted: m });
 
